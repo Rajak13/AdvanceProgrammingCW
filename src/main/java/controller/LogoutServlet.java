bbb400e249b8735cf5b120df70026520aa1e9ee0
Rajak13 <queresma05@gmail.com>
2025-09-17 10:38:47 +0545

    Re-Fixed for deployment#7

--- src/main/java/controller/LogoutServlet.java
+++ src/main/java/controller/LogoutServlet.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/auth/logout")
 public class LogoutServlet extends HttpServlet {
--- src/main/java/controller/PaymentServlet.java
+++ src/main/java/controller/PaymentServlet.java
@@ -5,12 +5,12 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 
 import dao.PaymentDAO;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import model.Payment;
 import model.User;
 
